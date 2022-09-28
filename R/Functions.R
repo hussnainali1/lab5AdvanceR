7f086a38e5cbf8e20240f24f5a1ea7c2fcddaeff
Hussnain Ali <110018587+iamhussnainn@users.noreply.github.com>
2022-09-28 23:33:00 +0200

    added new updation

---
+++ R/Functions.R
@@ -0,0 +1,54 @@
+library(httr)
+library(jsonlite)
+library(future)
+
+baseUrl <- "https://api.weatherapi.com/v1/"
+apiKey <- "148193efcfce480cb9591848222809"
+
+#' FetchWeatherData
+#'
+#' @param queryCity A String
+#'
+#' @return DataFrame
+#' @export FetchWeatherData
+#'
+#' @examples
+#' fetchWeatherData("pakistan")
+#'
+fetchWeatherData <- function(queryCity) {
+  stopifnot(is.character(queryCity))
+
+  url <- paste(baseUrl,"forecast.json?key=",apiKey, "&q=",queryCity, sep="")
+  getFutureRes <- future(GET(url))
+  resValue <- value(getFutureRes)
+  content <- content(resValue, "text")
+  dat <- jsonlite::fromJSON(content, flatten = TRUE)$forecast$forecastday$hour
+  dataframeObj <- as.data.frame(dat)[ ,2:3]
+  return(dataframeObj)
+}
+
+
+#' FetchCurrentWeather
+#'
+#' @param queryCity A String
+#'
+#' @return A DataFrame
+#' @export FetchCurrentWeather
+#'
+#' @examples
+#' fetchCurrentWeather("Pakistan")
+#'
+fetchCurrentWeather <- function(queryCity) {
+
+  stopifnot(is.character(queryCity))
+
+  url <- paste(baseUrl,"current.json?key=",apiKey, "&q=",queryCity, sep="")
+  getFutureRes <- future(GET(url))
+  resValue <- value(getFutureRes)
+  content <- content(resValue, "text")
+  dat <- jsonlite::fromJSON(content, flatten = TRUE)$current$temp_c
+  dataframeObj <- as.data.frame(dat)
+  colnames(dataframeObj) <- c("Current Temperature In Degree")
+  return(dataframeObj)
+}
+
--- tests/testthat/test-Functions.R
+++ tests/testthat/test-Functions.R
@@ -1,3 +1,32 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+library(testthat)
+library(httr)
+library(jsonlite)
+library(future)
+
+test_that("Input check doesnot works for fetchWeatherData()", {
+  expect_error(fetchWeatherData(12))
 })
+
+
+test_that("Output check doesnot works for fetchWeatherData()", {
+  result <- fetchWeatherData("sweden")
+  expect_true(is.data.frame(result))
+  expect_true(is.data.frame(result))
+  expect_equal(colnames(result)[1],"time")
+})
+
+
+test_that("Load check doesnot works for fetchWeatherData()", {
+
+  a <- 10
+  repeat {
+    print(fetchWeatherData("pakistan"))
+    a <- a - 1
+    if (a < 1) {
+      testthat::expect_true(is.data.frame(fetchWeatherData("pakistan")))
+      break;
+    }
+  }
+})
+
+
